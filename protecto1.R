58aa584f730f720dc8404c6525c9b889b65aa8e7
Pablo Viana <via16091@uvg.edu.gt>
2019-07-20 23:04:21 -0600

    combinacion de datos

---
+++ protecto1.R
@@ -0,0 +1,8 @@
+"Universidad del Valle de Guatemala - Data science 1 
+Catedratica: Lynette Garcia
+Pablo Viana - 16091 Sergio Marchena - 16 
+Jose Martinez -  Odalis Reyes -
+Ivan Maldonado - Mayra Silva - 
+
+----------------------------------* Proyecto 1 - Data Science *----------------------------------"
+load("republica_guatemala.RData")
